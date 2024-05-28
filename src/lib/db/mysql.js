211acfe16e4e941dde85cf3d0398723465f08846
dimah02 <dimahmehdawi@gmail.com>
2024-05-28 22:43:31 +0300

    Rename environment variables

--- src/lib/db/mysql.js
+++ src/lib/db/mysql.js
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import mysql from 'mysql2'
-import { USER, PASSWORD, HOST,DATABASE } from '$env/static/private';
+import { DB_USERNAME, DB_PASSWORD, HOST,DB_NAME } from '$env/static/private';
 import dotenv from 'dotenv'
 dotenv.config();
 
@@ -9,10 +9,10 @@ let mysqlconn = null;
 export function mysqlconnFn(){
     if (mysqlconn === null) {
         mysqlconn = mysql.createPool({
-            user: USER,
-            password:PASSWORD,
+            user: DB_USERNAME,
+            password:DB_PASSWORD,
             host:HOST,
-            database:DATABASE
+            database:DB_NAME
         }).promise()
     }
     return mysqlconn;
